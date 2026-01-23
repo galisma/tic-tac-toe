7da8c7ab0f52fa944431d4d0bc34a3328aefc1e8
Ismael Gallo <xigalmun600@ieshnosmachado.org>
2026-01-23 12:31:25 +0100

    able to join games

--- client-web/scripts.js
+++ client-web/scripts.js
@@ -17,30 +17,40 @@ const GameState = {
     board: ['', '', '', '', '', '', '', '', ''],
 }
 
-// Square clicks
+// Handlers
 const handleClick = (number) => {
-    if (GameState.state === "playing" && GameState.board[number-1] === ''){
-        message = {
-            type:"movement",
-            square: number,
-        }
-        socket.send(JSON.stringify(message));
-        console.log(`click in square ${number}`);
+    if (GameState.state === "playing" && GameState.board[number] === '' && GameState.turn === true ) {
+        socket.send(JSON.stringify({type:"movement",square:number}));
+        console.log(`Clicked square ${number}`);
     }
 };
 
+const hanldleMatch = (data) => {
+    console.log('Match found');
+    GameState.state = "playing";
+    GameState.symbol = data.symbol;
+    if (GameState.symbol === 'X') {
+        display.innerHTML = "Tu turno"
+        GameState.turn = true;
+    } else {
+        display.innerHTML = "Turno del rival";
+    }
+}
+
 document.querySelectorAll('.square').forEach((square, index) => {
-    square.addEventListener('click', () => handleClick(index + 1));
+    square.addEventListener('click', () => handleClick(index));
 });
 
 // Searching match
 searchBtn.addEventListener('click', () => {
-
     GameState.state = "searching";
     display.innerHTML = "Buscando partida...";
-    const message = {
-        type:"search",
-    }
-    socket.send(JSON.stringify(message));
+    socket.send(JSON.stringify({type:"search"}));
 });
 
+socket.onmessage = (event) => {
+    const data = JSON.parse(event.data);
+    if (data.type == "match"){
+        hanldleMatch(data);
+    }
+}
