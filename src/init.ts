f7640c756c3e83d2d4951404da35869fc31dd351
Galisma <igallomunoz@proton.me>
2026-01-24 15:08:18 +0100

    Add handlemove()

--- src/init.ts
+++ src/init.ts
@@ -1,36 +1,37 @@
 import { WebSocketServer, WebSocket } from 'ws';
 import { v4 as uuidv4 } from 'uuid';
 
+type Square = 'X' | 'O' | '';
 declare module 'ws' {
     interface WebSocket {
         roomId?: string;
-        sylbol?: string;
+        symbol?: Square;
     }
 }
 
 const wss = new WebSocketServer({ port: 8080 });
 
-type Square = 'X' | 'O' | '';
 let waitingRoom: string | null = null;
 const rooms: Map<string, {
     players: WebSocket[];
     board: Square[];
-    turn: string;
+    turn: Square;
 }> = new Map();
 
-wss.on('connection', (ws) => {
-    ws.on('error', console.error);
-    ws.on('message', (packet) => {
-        let data = JSON.parse(packet.toString());
-        switch (data.type) {
-            case "search": handleSearch(ws);
-            case "movement": handleMove(ws, data);
-        }
-    });
-});
+let handleMove = (player: WebSocket, data: any) => {
+    const room = rooms.get(player.roomId || '');
 
-let handleSearch = (player:WebSocket) => {
-    if (waitingRoom === null){ 
+    if(!room) return;
+
+    if (room.turn === player.symbol && room.board[data.square] === '') {
+        room.board[data.square] = player.symbol;
+        room.turn = room.turn === 'X' ? 'O' : 'X';
+        room.players[player.symbol === 'X' ? 1 : 0].send(JSON.stringify(data));
+    }
+}
+
+let handleSearch = (player: WebSocket) => {
+    if (waitingRoom === null) {
         const roomId = uuidv4();
         rooms.set(roomId, {
             players: [player],
@@ -39,7 +40,7 @@ let handleSearch = (player:WebSocket) => {
         });
 
         waitingRoom = roomId;
-        player.roomId=roomId;
+        player.roomId = roomId;
 
         console.log(`Sala ${roomId} creada. Jugador 1 esperando`)
     } else {
@@ -48,13 +49,25 @@ let handleSearch = (player:WebSocket) => {
 
         if (room) {
             room.players.push(player);
-            player.roomId=roomId;
+            player.roomId = roomId;
             waitingRoom = null;
             console.log(`Partida lista en la sala: ${roomId}`);
-            room.players[0].send(JSON.stringify({type:'match', symbol:'X'}));
-            room.players[1].send(JSON.stringify({type:'match', symbol:'0'}));
+            room.players[0].send(JSON.stringify({ type: 'match', symbol: 'X' }));
+            room.players[1].send(JSON.stringify({ type: 'match', symbol: '0' }));
+
+            room.players[0].symbol = 'X';
+            room.players[1].symbol = 'O';
         }
     }
 }
 
-// let handleMove = (player:WebSocket, data:string) {}
+wss.on('connection', (ws) => {
+    ws.on('error', console.error);
+    ws.on('message', (packet) => {
+        let data = JSON.parse(packet.toString());
+        switch (data.type) {
+            case "search": handleSearch(ws); break;
+            case "movement": handleMove(ws, data); break;
+        }
+    });
+});
