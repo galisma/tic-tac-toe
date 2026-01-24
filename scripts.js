0fe01d7801072807827b887723c5f9e4b31e2511
Galisma <igallomunoz@proton.me>
2026-01-24 15:56:25 +0100

    multiple fixes

--- scripts.js
+++ scripts.js
@@ -3,7 +3,8 @@ const board = document.getElementById("board");
 const searchBtn = document.getElementById("search");
 
 // Websocket
-const socket = new WebSocket(`ws://${window.location.hostname}:8080`);
+const host = window.location.hostname || "localhost";
+const socket = new WebSocket(`ws://${host}:8080`);
 socket.onopen = () => {
     console.log("Websocket connected");
 };
@@ -18,10 +19,22 @@ const GameState = {
 }
 
 // Handlers
+const handleMovement = (data) => {
+    console.log(`received ${data.type} ${data.square}`)
+    GameState.turn = true;
+    display.innerHTML = "Tu turno";
+
+    const opponent_symbol = GameState.symbol === 'X' ? 'O' : 'X';
+    GameState.board[data.square] = opponent_symbol;
+    document.getElementById(`sq-${data.square}`).innerHTML = opponent_symbol;
+}
+
 const handleClick = (number) => {
     if (GameState.state === "playing" && GameState.board[number] === '' && GameState.turn === true ) {
+        document.getElementById(`sq-${number}`).innerHTML = GameState.symbol;
+        GameState.turn = false
+        display.innerHTML = "Turno del rival"
         socket.send(JSON.stringify({type:"movement",square:number}));
-        console.log(`Clicked square ${number}`);
     }
 };
 
@@ -29,6 +42,8 @@ const hanldleMatch = (data) => {
     console.log('Match found');
     GameState.state = "playing";
     GameState.symbol = data.symbol;
+
+    searchBtn.style.display = 'none';
     if (GameState.symbol === 'X') {
         display.innerHTML = "Tu turno"
         GameState.turn = true;
@@ -43,6 +58,10 @@ document.querySelectorAll('.square').forEach((square, index) => {
 
 // Searching match
 searchBtn.addEventListener('click', () => {
+    if (socket.readyState !== 1){
+        console.log("Error conectando con el servidor")
+        return;
+    }
     GameState.state = "searching";
     display.innerHTML = "Buscando partida...";
     socket.send(JSON.stringify({type:"search"}));
@@ -50,7 +69,9 @@ searchBtn.addEventListener('click', () => {
 
 socket.onmessage = (event) => {
     const data = JSON.parse(event.data);
-    if (data.type == "match"){
-        hanldleMatch(data);
+    switch (data.type){
+        case "match": hanldleMatch(data); break;
+        case "movement": handleMovement(data); break;
+        default: console.log(`Error, received ${data.type}`)
     }
 }
