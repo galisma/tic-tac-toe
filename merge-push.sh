19183f2bde5f264a3b270fd9d57de1004e04a11c
Galisma <igallomunoz@proton.me>
2026-01-25 12:18:26 +0100

    updated script

--- merge-push.sh
+++ merge-push.sh
@@ -3,20 +3,25 @@ cd /home/ismael/Proyectos/tic-tac-toe/main
 
 function sync_readme() {
     local branch=$1
+    echo "Sincronizando README en rama: $branch"
     git checkout "$branch"
     git checkout main -- README.md
-    git commit -m "Update README from main"
+    git add README.md
+    git diff-index --quiet HEAD || git commit -m "Update README from main"
     git push origin "$branch"
+    git checkout main
 }
 
 function merge() {
     git merge -s subtree server -m "Merge server"
     git merge -s subtree client-web -m "Merge client-web"
+    git merge -s subtree client-tui -m "Merge client-tui"
 }
 
 function push() {
     sync_readme server
     sync_readme client-web
+    sync_readme client-tui
 
     git checkout main
     git push origin main
